507346428474b3b3b9efdd8bfb2fbc5712ccd415
Ulises-FAI <ulises.corrales@est.fi.uncoma.edu.ar>
2024-05-22 19:07:07 -0300

    realizando el punto 2

--- tp4/tp4punto2.c
+++ tp4/tp4punto2.c
@@ -30,28 +30,32 @@ typedef struct {
 	unsigned int colorsImportant;
 } BMPInfoHeader;
 #pragma pack(pop)
-
 unsigned char nueva_imagen [2000*2000*3];
-
-void convertir(int in_fd, int out_fd, BMPHeader h, BMPInfoHeader infoh,int comienzoFila,int finFila){
+void convertir( BMPHeader h, BMPInfoHeader infoh,int comienzoFila,int finFila){
     int width = infoh.width;
 	int height = infoh.height;
 	unsigned char gray;
 	int padding = (4 - (width * 3) % 4) % 4; // Calculating padding size
 	unsigned char pixel[3];
 	
+	int or=abrirArchivoOr();
+    int tmpGray=abrirArchivoTmp();
     for(int i=comienzoFila;i<finFila;i++){
         for(int j=0;j<width;j++){
-            read(in_fd, pixel, 3);
+            read(or,pixel, 3);
 	        gray = (unsigned char)(0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0]);
 	        nueva_imagen[i*(width*3+padding) + j*3]= gray;
 	        nueva_imagen[i*(width*3+padding) + j*3 + 1] = gray;
 	        nueva_imagen[i*(width*3+padding) + j*3 + 2] = gray;
         }
-   		lseek(in_fd, padding, SEEK_CUR); // Skipping padding
+   		lseek(nueva_imagen, padding, SEEK_CUR); // Skipping padding
     }
+    write(tmpGray, &h, sizeof(BMPHeader));
+    write(tmpGray, &infoh, sizeof(BMPInfoHeader));
+    write(tmpGray, &nueva_imagen[0], infoh.width * infoh.height * 3);
+    close(tmpGray);
+    close(or);
 }
-
 void something_wrong(int fd, const char *m)
 {
 	if (fd > 0)
@@ -59,10 +63,16 @@ void something_wrong(int fd, const char *m)
 	printf("%s\n", m),
 	exit(1);
 }
-
-int main() 
-{
-	int in_fd = open(BMP_FILE, O_RDONLY);
+int abrirArchivoOr(){
+    int in_fd = open(BMP_FILE, O_RDONLY);
+	if (in_fd < 0) {
+		something_wrong(in_fd, "Error open");
+	}
+	return in_fd;
+}
+int abrirArchivoTmp(){
+    int in_fd = open(GRAYSCALE_FILE, O_RDWR | O_CREAT, 0644);
+    
 	if (in_fd < 0) {
 		something_wrong(in_fd, "Error open");
 	}
@@ -71,7 +81,43 @@ int main()
 	read(in_fd, &h, sizeof(BMPHeader));
 
 	if (h.magic[0] != 'B' || h.magic[1] != 'M') {
-		something_wrong(in_fd, "No es BMP");
+		something_wrong(in_fd, "No es BMP"); 
+	}
+
+	BMPInfoHeader infoh;
+	read(in_fd, &infoh, sizeof(BMPInfoHeader));
+
+	if (infoh.bpp != 24 || infoh.compression != 0) {
+		something_wrong(in_fd, "No 24-bit BMP");
+	}
+    //cargar píxeles en el arreglo    
+    unsigned char pixel[3];
+    int width=infoh.width;
+    int height=infoh.height;
+  	int padding = (4 - (width * 3) % 4) % 4; // Calculating padding size
+	for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            read(in_fd, pixel, 3);
+	        nueva_imagen[i*(width*3+padding) + j*3]= pixel[0];
+	        nueva_imagen[i*(width*3+padding) + j*3 + 1] = pixel[1];
+	        nueva_imagen[i*(width*3+padding) + j*3 + 2] = pixel[2];
+        }
+    }
+    	
+	return in_fd;
+}
+int main()
+{	
+    int in_fd = open(BMP_FILE, O_RDONLY);
+	if (in_fd < 0) {
+		something_wrong(in_fd, "Error open");
+	}
+
+	BMPHeader h;
+	read(in_fd, &h, sizeof(BMPHeader));
+
+	if (h.magic[0] != 'B' || h.magic[1] != 'M') {
+		something_wrong(in_fd, "No es BMP"); 
 	}
 
 	BMPInfoHeader infoh;
@@ -81,43 +127,40 @@ int main()
 		something_wrong(in_fd, "No 24-bit BMP");
 	}
 
-	int out_fd = open(GRAYSCALE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    //Guardar una primera imagen temporal
+
+	int out_fd = open(GRAYSCALE_FILE, O_WRONLY | O_CREAT , 0644);
+	write(out_fd, &h, sizeof(BMPHeader));
+    write(out_fd, &infoh, sizeof(BMPInfoHeader));
+    write(out_fd, &nueva_imagen[0], infoh.width * infoh.height * 3);
+    close(in_fd);
+    close(out_fd);
+    
 	if (out_fd < 0) {
 		something_wrong(in_fd, "error en output");
 	}
-
-    printf("heigth: %d, width: %d\n",infoh.height,infoh.width);
-    //Separar convertir en 3 proceso
-  	//convertir(in_fd, out_fd, h, infoh);
+    //Separar convertir en 3 procesos
   	int a=fork();
   	if(a==0){
-  	    convertir(in_fd, out_fd, h, infoh,0,354);
+  	    convertir(h, infoh,0,354);
   	    exit(0);
-  	}else{
+  	}/*else{
+ 	    wait(a);
   	    int b=fork();
   	    if(b==0){
-      	    convertir(in_fd, out_fd, h, infoh,354,708);
+      	    convertir(h, infoh,354,708);
       	    exit(0);
   	    }else{
+  	        wait(b);
   	        int c=fork();
   	        if(c==0){
-  	            convertir(in_fd, out_fd, h, infoh,708,1063);
+  	            convertir(h, infoh,708,1063);
           	    exit(0);
   	        }else{
-  	            wait(a);
-  	            wait(b);
   	            wait(c);
-  	            
-            	write(out_fd, &h, sizeof(BMPHeader));
-	            write(out_fd, &infoh, sizeof(BMPInfoHeader));
-	            write(out_fd, &nueva_imagen[0], infoh.width * infoh.height * 3);
-
-	            close(in_fd);
-	            close(out_fd);
-	            printf("Imagen en gris generada. %s\n", GRAYSCALE_FILE);
-	            exit(0);
+                printf("Imagen en gris generada. %s\n", GRAYSCALE_FILE);
   	        }
   	    }
-  	}
+  	}*/
 }
 
